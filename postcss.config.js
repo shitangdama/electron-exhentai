483317e0f348dcc1c7edea5e3a148612d07d3446
zertu <290055513@qq.com>
2017-03-05 16:35:48 +0800

    update code

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-  plugins: [
-    // require('postcss-smart-import')({ /* ...options */ }),
-    // require('precss')({ /* ...options */ }),
-    autoprefixer({ /* ...options */ }),
-  ],
-};
--- renderer.js
+++
@@ -1,8 +0,0 @@
-
-// const remote = require('electron').remote;
-
-// const request = remote.require('request');
-
-export default function test() {
-  console.log('11111111111111111');
-}
